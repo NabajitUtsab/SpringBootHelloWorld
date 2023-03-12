a8dc11b9298239b86ba4d9cb3a6ac5cb5ecab83d
NabajitUtsab <88704676+NabajitUtsab@users.noreply.github.com>
2023-03-13 01:28:26 +0600

    Add files via upload

---
+++ Task2HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.exampleForTask2.Task2Hello.World;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task2HelloWorldApplication.class, args);
+	}
+
+}
